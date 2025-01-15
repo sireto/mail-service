49f91b4709dfa53ae9f391e1e14daa501b1e0b77
kanak1125 <kanak24.fv@gmail.com>
2025-01-15 20:51:03 +0545

    setup: project setup with prisma setup

---
+++ backend/src/handler.rs
@@ -0,0 +1,19 @@
+/**
+ * @description a function to get all the templates...
+ */
+pub async fn get_templates() {}
+
+/**
+ * @description a function to create a template...
+ */
+pub async fn create_template() {}
+
+/**
+ * @description a function to update a template...
+*/
+pub async fn update_template(templateId: String) {}
+
+/**
+ * @description a function to delete a template...
+ */
+pub async fn delete_template(templateId: String) {}
