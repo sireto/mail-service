402c2efa12a7749886dbe8bb49b64c7e70978e31
saisab29 <saisabk29@gmail.com>
2025-01-24 11:33:28 +0545

    Create docker compose and initial data migration

--- backend/migrations/2025-01-19-115851_create_templates/up.sql
+++ backend/migrations/2025-01-19-115851_create_templates/up.sql
@@ -15,3 +15,15 @@ CREATE TABLE "templates" (
 ALTER TABLE "templates"
     ADD CONSTRAINT "template_namespace_id_fkey" FOREIGN KEY ("namespace_id")
     REFERENCES "namespaces"("id") ON DELETE CASCADE;
+
+
+INSERT INTO "templates" (namespace_id, name, template_data, content_plaintext, content_html, created_at, updated_at)
+VALUES (
+    'e3bda5cf-760e-43ea-8e9a-c2c3c5f95b82', 
+    'Welcome Email 001', 
+    '{"key1": "value1", "key2": "value2"}', 
+    'Hello, welcome to our service!', 
+    '<h1>Hello</h1><p style="display: flex;">Welcome to our service!</p>', 
+    '2025-01-24 05:15:21.356911+00', 
+    '2025-01-24 05:15:21.356911+00'
+);
